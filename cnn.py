978e4f676115fc6a58b7156afb335ddafe21891e
Ayush <52930904+cybergodayush@users.noreply.github.com>
2020-05-21 01:10:57 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -1,24 +1,48 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.regularizers import l2
 from keras.datasets import mnist
-datasets = mnist.load_data(mnist.db)
-train , test = datasets
-xtrain , xtrain = train
-xtest , ytest = test
-img_rows = xtrain[0].shape[0]
-img_cols = xtrain[1].shape[0]
-xtrain = xtrain.reshape(xtrain.shape[0], img_rows, img_cols, 1)
-xtest = xtest.reshape(xtest.shape[0], img_rows, img_cols, 1)
+from keras.utils import np_utils
+import  keras
+import random
+#addhere
+
+# loads the MNIST dataset
+(x_train, y_train), (x_test, y_test)  = mnist.load_data()
+
+# Lets store the number of rows and columns
+img_rows = x_train[0].shape[0]
+img_cols = x_train[1].shape[0]
+
+# Getting our date in the right 'shape' needed for Keras
+# We need to add a 4th dimenion to our date thereby changing our
+# Our original image shape of (60000,28,28) to (60000,28,28,1)
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+
+# store the shape of a single image
 input_shape = (img_rows, img_cols, 1)
-xtrain = xtrain.astype('float32')
-xtest = xtest.astype('float32')
-xtrain /= 255
-xtest /= 255
-ytrain = np_utils.to_categorical(ytrain)
-ytest = np_utils.to_categorical(ytest)
-num_classes = ytest.shape[1]
-num_pixels = xtrain.shape[1] * xtrain.shape[2]
+
+# change our image type to float32 data type
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+# Normalize our data by changing the range from (0 to 255) to (0 to 1)
+x_train /= 255
+x_test /= 255
+
+# Now we one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+num_classes = y_test.shape[1]
+num_pixels = x_train.shape[1] * x_train.shape[2]
 
 
-# create model
+#create model
 model = Sequential()
 model.add(Conv2D(20, (5, 5),
                  padding = "same",
@@ -26,6 +50,8 @@ model.add(Conv2D(20, (5, 5),
 
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2)))
+
+#Addnewconv2Dlayerhere
 
 # Fully connected layers (w/ RELU)
 model.add(Flatten())
@@ -42,10 +68,12 @@ model.compile(loss = 'categorical_crossentropy',
 
 
 batch_size = 128
-epochs = 10
+epochs = 1
 
-model.fit(xtrain, ytrain,
+model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
-          validation_data=(xtest, ytest),
+          validation_data=(x_test, y_test),
           shuffle=True)
+
+model.save('MNIST_cnn.h5')
